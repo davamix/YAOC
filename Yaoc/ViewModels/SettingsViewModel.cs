620a0f70a80bdfd740a5b052fabfa133bea53046
Daniel Valcarce <davamix@hotmail.com>
2025-03-01 02:22:48 +0100

    Added tabs on Settings view. Added  General and Plugins settings

--- Yaoc/ViewModels/SettingsViewModel.cs
+++ Yaoc/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Configuration;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+using Yaoc.Core.Models;
+using Yaoc.Core.Plugins;
 using Yaoc.Core.Services;
 
 namespace Yaoc.ViewModels;
@@ -15,6 +19,8 @@ public partial class SettingsViewModel : ObservableObject {
     [ObservableProperty]
     private bool? _isConnectionValid;
 
+    public ObservableCollection<Plugin> PluginsAvailable { get; private set; } = new();
+
     public SettingsViewModel(
         IOllamaService ollamaService,
         IConfiguration configuration) {
@@ -23,12 +29,23 @@ public partial class SettingsViewModel : ObservableObject {
         _configuration = configuration;
 
         OllamaServerUrl = _configuration["AppSettings:OllamaServer"];
+
+        LoadPluginsAvailable();
     }
 
     [RelayCommand]
     public async Task TestConnection() {
         IsConnectionValid = await _ollamaService.TestConnection(OllamaServerUrl);
         RefreshProperty(nameof(IsConnectionValid));
+    }
+
+    private void LoadPluginsAvailable() {
+        PluginsAvailable.Clear();
+        var plugins = PluginsLoader.GetFileParserPlugins().Values.Select(x => new { Name = x.Name, Description = x.Description });
+
+        foreach(var p in plugins) {
+            PluginsAvailable.Add(new Plugin(p.Name, p.Description));
+        }
     }
 
     private void RefreshProperty(string propertyName) {
